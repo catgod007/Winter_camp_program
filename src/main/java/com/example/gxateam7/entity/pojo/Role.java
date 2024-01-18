5abe5ae33126e9d60d25e8ec9bf29576d0b1bade
cat-god-007 <2337563298@qq.com>
2024-01-18 15:57:52 +0800

    修改与增加未动，下拉框已完成；删除与查询已完成

---
+++ src/main/java/com/example/gxateam7/entity/pojo/Role.java
@@ -0,0 +1,35 @@
+package com.example.gxateam7.entity.pojo;
+
+public class Role {
+  private Integer roleId;
+  private String rolename;
+  private String roleremark;
+
+
+  public Integer getRoleId() {
+    return roleId;
+  }
+
+  public void setRoleId(Integer roleId) {
+    this.roleId = roleId;
+  }
+
+
+  public String getRolename() {
+    return rolename;
+  }
+
+  public void setRolename(String rolename) {
+    this.rolename = rolename;
+  }
+
+
+  public String getRoleremark() {
+    return roleremark;
+  }
+
+  public void setRoleremark(String roleremark) {
+    this.roleremark = roleremark;
+  }
+
+}
