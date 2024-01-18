09ef5923327f6b4d0bea2fc85bd9726a307fdbd8
h2923262014 <2923262014@qq.com>
2024-01-18 19:47:32 +0800

    教师信息管理第一版

---
+++ src/main/java/com/example/gxateam7/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.gxateam7.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hzc
+ * Date: 2024/01/1717:01
+ * Description:
+ * Version: V1.0
+ */
+public class TestController {
+    
+}
